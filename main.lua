a06f3096c86633802b2fb48c7eb5926213097a87
Drew Whitmer <aswhitme@ucsc.edu>
2025-04-18 21:03:18 -0700

    cleaned up some code, added the 7 stacks for solitaire

--- main.lua
+++ main.lua
@@ -16,23 +16,38 @@ function love.load()
   
   --put all cards in one table
   for i = 1, 13 do
-    table.insert(cardTable1, CardClass:new('clubs',i,false,true,false,100,100))
+    table.insert(cardTable1, CardClass:new('clubs',i,true,true,false,0,0))
   end
   for i = 1, 13 do
-    table.insert(cardTable1, CardClass:new('hearts',i,false,true,false,100,100))
+    table.insert(cardTable1, CardClass:new('hearts',i,true,true,false,0,0))
   end
   for i = 1, 13 do
-    table.insert(cardTable1, CardClass:new('spades',i,false,true,false,100,100))
+    table.insert(cardTable1, CardClass:new('spades',i,true,true,false,0,0))
   end
   for i = 1, 13 do
-    table.insert(cardTable1, CardClass:new('diamonds',i,false,true,false,100,100))
+    table.insert(cardTable1, CardClass:new('diamonds',i,true,true,false,0,0))
   end
   
-  stack1 = StackClass:new(cardTable1, 100, 100)
   stackTable = {}
   
-  table.insert(stackTable, stack1)
-  stackTable[1]:refresh()
+  table.insert(stackTable, StackClass:new({}, 100, 100, 10))
+  table.insert(stackTable, StackClass:new({}, 200, 100, 10))
+  table.insert(stackTable, StackClass:new({}, 300, 100, 10))
+  table.insert(stackTable, StackClass:new({}, 400, 100, 10))
+  table.insert(stackTable, StackClass:new({}, 500, 100, 10))
+  table.insert(stackTable, StackClass:new({}, 600, 100, 10))
+  table.insert(stackTable, StackClass:new({}, 700, 100, 10))
+  
+  for i = 1, 7 do
+    for j = 1, i do
+      local num = math.random(#cardTable1)
+      local card = cardTable1[num]
+      table.remove(cardTable1, num)
+      table.insert(stackTable[i].cards, card)
+    end
+    stackTable[i]:update()
+  end
+  
   
 end
 
@@ -42,8 +57,6 @@ function love.update()
   
   for _, stack in ipairs(stackTable) do
     for index, card in ipairs(stack.cards) do
-
-      card:update()
       --checks if grab is happening and if the grab position is on the card
       if grabber.grabPos ~= nil and grabber.grabPos.x >= card.pos.x and grabber.grabPos.x <= card.pos.x + CARD_WIDTH and grabber.grabPos.y >= card.pos.y and grabber.grabPos.y <= card.pos.y + CARD_HEIGHT and card.grabbable == true and card.grabbed ~= true then 
         -- sets current card to grabbed
@@ -62,20 +75,15 @@ function love.update()
         --lets go of the card when the player releases
         card.grabbed = false
         for _, newStack in ipairs(stackTable) do 
-          if grabber.currentMousePos.x >= newStack.pos.x and grabber.currentMousePos.x <= newStack.pos.x + CARD_WIDTH and grabber.currentMousePos.y >= newStack.pos.y and grabber.currentMousePos.y <= newStack.pos.y + ((#newStack.cards)*10 + CARD_HEIGHT) then
+          if grabber.currentMousePos.x >= newStack.pos.x and grabber.currentMousePos.x <= newStack.pos.x + CARD_WIDTH and grabber.currentMousePos.y >= newStack.pos.y and grabber.currentMousePos.y <= newStack.pos.y + ((#newStack.cards)*newStack.offset + CARD_HEIGHT) then
             table.remove(stack.cards, index)
             table.insert(newStack.cards, card)
-            print("true")
           end
-          newStack:refresh()
-          stack:refresh()
         end
-      elseif card.grabbed == true then
-        --moves the card to the current mouse position if it is grabbed
-        
-        card.pos = grabber.currentMousePos
       end
+      card:update()
     end
+    stack:update()
   end
 
 end
--- stack.lua
+++ stack.lua
@@ -3,22 +3,34 @@ StackClass = {}
 require "vector"
 require "card"
 
-function StackClass:new(cardTable, xPos, yPos)
+function StackClass:new(cardTable, xPos, yPos, off)
   stack = {}
   local metadata = {__index = StackClass}
   setmetatable(stack, metadata)
   
   stack.pos = Vector(xPos, yPos)
   stack.cards = cardTable
+  stack.offset = off
   for index, card in ipairs(stack.cards) do
-    card.pos = Vector(stack.pos.x, stack.pos.y + (10*index))
+    card.pos = Vector(stack.pos.x, stack.pos.y + (stack.offset*index))
   end
   return stack
 end
 
-function StackClass:refresh()
+function StackClass:update()
   for index, card in ipairs(self.cards) do
-    card.pos = Vector(self.pos.x, self.pos.y + (10*index))
+    if not card.grabbed then
+      card.pos = Vector(self.pos.x, self.pos.y + (self.offset*index))
+    end
+    
+    if card.flipped then
+      card.grabbable = false
+    else
+      card.grabbable = true
+    end
+    
+  end
+  if #self.cards > 0 and self.cards[#self.cards].flipped then
+    self.cards[#self.cards].flipped = false
   end
 end
-  
